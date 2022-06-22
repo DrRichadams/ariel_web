6e4f6c4ab558795299852bacd809829d8b60efc9
DrRichadams <36617473+DrRichadams@users.noreply.github.com>
2022-06-22 13:17:23 +0200

    Init commit

---
+++ src/constants/colors.js
@@ -0,0 +1,12 @@
+export const colors = {
+    primary: '#0F0E1C',
+    accent: 'FF8A00',
+    bg_color: '#fff',
+    menu_color: '#D9D9D9',
+    success: '#3BB54A',
+}
+
+export const opacities = {
+    bg_opacity: 0.3,
+    bg_effect_opacity: 0.2
+}
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styled from "styled-components";
+
+const HomePage = () => {
+    return(
+        <div>
+            Home
+        </div>
+    )
+}
+
+export default HomePage;
