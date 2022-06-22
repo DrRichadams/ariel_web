f4a64944c48f478a73f642f748ca7c18e44fd88f
DrRichadams <36617473+DrRichadams@users.noreply.github.com>
2022-06-22 19:32:29 +0200

    Periodic commit

--- src/features/SectionContainer.js
+++ src/features/SectionContainer.js
@@ -3,14 +3,15 @@ import styled from "styled-components";
 import { colors } from "../constants/colors";
 
 const MainContainer = styled.div`
-    padding: 30px 100px;
-    background-color: ${colors.primary};
+    padding: 30px 150px;
+    /* background-color: ${colors.primary}; */
+    background-color: ${props => props.color};
     min-height: 100vh;
 `;
 
-export const SectionContainer = ({children}) => {
+export const SectionContainer = ({children, bg_color}) => {
     return(
-        <MainContainer>
+        <MainContainer color={bg_color}>
             {children}
         </MainContainer>
     )
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,13 +2,29 @@ import React from "react"
 import styled from "styled-components";
 import HomeMenu from "../components/HomeMenu";
 import { SectionContainer } from "../features/SectionContainer";
+import { colors } from "../constants/colors";
 
 const HomePage = () => {
     return(
-        <SectionContainer>
-            <HomeMenu />
-            <div>HOME PAGE</div>
-        </SectionContainer>
+        <div className="home_page_">
+            <SectionContainer bg_color={colors.primary}>
+                <HomeMenu />
+                <div className="home_banner">
+                    <div className="left_sect">
+                        <h1>Ariel Crypto Management</h1>
+                        <h2>Your #1 investment platform</h2>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        tempor incididunt ut labore et dolore magna aliqua 
+                        Ut enim ad minim veniam,
+                        </p>
+                    </div>
+                    <div className="right_sect">
+                        <img src={process.env.PUBLIC_URL + "/images/banner_pic.png"} alt="" />
+                    </div>
+                </div>
+            </SectionContainer>
+        </div>
     )
 }
 
