11e7898ce9d0bcf9631154ec3fc3cd5fc5d83e66
DrRichadams <36617473+DrRichadams@users.noreply.github.com>
2022-06-28 19:52:56 +0200

    Periodic commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,17 @@
 import React from "react"
 import styled from "styled-components";
+import { colors } from "../constants/colors";
+import { SectionContainer } from "../features/SectionContainer";
+import { FiArrowLeft } from "react-icons/fi"
+import { BsArrowLeft } from "react-icons/bs"
 
 const Login = () => {
     return(
-        <div>
-            Login
-        </div>
+        <SectionContainer size={100} bg_color={colors.primary}>
+            <div className="top_bar">
+                <BsArrowLeft color="#fff" size={30} />
+            </div>
+        </SectionContainer>
     )
 }
 
