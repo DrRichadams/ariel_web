ab7f4446076d19339789359810f467f876b59e3c
DrRichadams <36617473+DrRichadams@users.noreply.github.com>
2022-07-01 00:27:34 +0200

    Periodic commit

--- src/DashboardUser/pages/InvestmentPlansPage.js
+++ src/DashboardUser/pages/InvestmentPlansPage.js
@@ -9,21 +9,21 @@ const InvestmentPlansPage = () => {
         <div>
             <CurrentPlan>
                 <TitleBar className="title_bar">
-                    <h4>Your current package</h4>
-                    <button>Request withdraw</button>
+                    <BarTitle>Your current package</BarTitle>
+                    <RequestBtn>Request withdraw</RequestBtn>
                 </TitleBar>
-                <div className="values_sect">
-                    <div className="package_sect">
-                        <h3>EMERALD</h3>
-                        <p>USD$ 300</p>
-                    </div>
-                    <div className="balance_sect">
-                        <p>Available balance</p>
-                        <p>USD$ 400 000</p>
-                    </div>
-                </div>
+                <ValuesSection className="values_sect">
+                    <PackageTypeContainer>
+                        <PackageType>EMERALD</PackageType>
+                        <PackageCost>USD$ 300</PackageCost>
+                    </PackageTypeContainer>
+                    <BalanceContainer>
+                        <AvailableBalance>Available balance</AvailableBalance>
+                        <ActualBalance>USD$ 400 000</ActualBalance>
+                    </BalanceContainer>
+                </ValuesSection>
                 <div className="recommendation_titles">
-                    <h3>Recommendations</h3>
+                    <RecommendTitle>Recommendations</RecommendTitle>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quasi explicabo, commodi nam eius ipsum deserunt soluta ipsam aut. Consequuntur mollitia laboriosam non quos numquam accusantium iste culpa porro minima.</p>
                 </div>
                 <div className="actual_recommendations">
@@ -41,11 +41,80 @@ const InvestmentPlansPage = () => {
     )
 };
 
+export const RecommendTitle = styled.div`
+    margin: 0;
+`;
+export const PackageTypeContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`;
+export const BalanceContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+`;
 
+export const ActualBalance = styled.p`
+    margin: 0;
+    color: #01050f;
+    font-weight: 900;
+    font-size: 20px;
+    font-family: Roboto;
+    margin-top: 2px;
+`;
+export const AvailableBalance = styled.h3`
+    margin: 0;
+    color: ${colors.accent};
+    font-size: 14px;
+`;
+export const PackageCost = styled.p`
+    margin: 0;
+    color: #01050f;
+    font-weight: 900;
+    font-size: 20px;
+    font-family: Roboto;
+    margin-top: 2px;
+`;
+export const PackageType = styled.h3`
+    margin: 0;
+    color: ${colors.accent};
+    font-size: 14px;
+`;
+export const ValuesSection = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+`;
+
+export const BarTitle = styled.h4`
+    text-transform: uppercase;
+    color: #01050f;
+    font-size: 20px;
+    font-family: Oswald, sans-serif;
+    font-weight: 900;
+    margin: 0;
+`;
+export const RequestBtn = styled.div`
+    cursor: pointer;
+    transition: all 250ms ease-in;
+    text-transform: uppercase;
+    border: 2px solid ${colors.accent};
+    color: ${colors.accent};
+    border-radius: 6px;
+    padding: 6px 8px;
+    font-size: 14px;
+    font-weight: 500;
+    :hover {
+        background-color: ${colors.accent};
+        color: #fff;
+    }
+`;
 export const TitleBar = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    margin-bottom: 20px;
 `;
 
 export const CurrentPlan = styled.div`
