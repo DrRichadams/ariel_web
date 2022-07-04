67116778cdede975545528fc43f43309bebdb856
DrRichadams <36617473+DrRichadams@users.noreply.github.com>
2022-07-05 01:28:32 +0200

    End of day commit

--- src/DashboardUser/features/Containers.js
+++ src/DashboardUser/features/Containers.js
@@ -1,11 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import { colors } from "../../constants/colors";
-import { BG_DOTS } from "../../constants/DATA";
+import BG_IMAGE from "../../resources/BG_IMAGE.jpg"
 
 export const RightContainer = styled.div`
-    /* background-color: red; */
-    background: linear-gradient(60deg, rgba(255, 138, 0, 0.3), rgba(255,0,0,0.2));
+    background-color: #01050f10;
+    /* background: linear-gradient(60deg, rgba(255, 138, 0, 0.3), rgba(255,0,0,0.2)); */
+    /* background-image: url("../../resources/BG_IMAGE.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center; */
     width: 100%;
     padding: 20px 100px;
     position: relative;
@@ -17,64 +21,15 @@ export const ChildrenContainer = styled.div`
     position: relative;
     z-index: 1000;
 `;
-export const CircleOutlineContainer = styled.div`
-    position: absolute;
-    overflow: hidden;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    display: flex;
-    gap: 5px;
-`;
 
-export const CircleOutline = styled.div`
-    width: 1px;
-    height: 100%;
-    /* border: 2px solid ${colors.accent}; */
-    background-color: ${colors.accent};
-    opacity: .1;
-`;
 
-export const LinesContainer = styled.div`
-    position: absolute;
-    overflow: hidden;
-    width: 100%;
-    height: 1500px;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    display: flex;
-    gap: 5px;
-    transform: rotate(${props=>props.sign}45deg) translateY(-300px);
-`;
 
-let BG_ARRAY = new Array(180).fill(0)
 
 
 const RightContainerComponent = ({children}) => {
     return(
         <RightContainer>
             <ChildrenContainer>{children}</ChildrenContainer>
-            <CircleOutlineContainer>
-                <LinesContainer sign="+">
-                    {
-                        BG_ARRAY.map((item, index) => (
-                            <CircleOutline />
-                        ))
-                    }
-                </LinesContainer>
-                <LinesContainer sign="-">
-                    {
-                        BG_ARRAY.map((item, index) => (
-                            <CircleOutline />
-                        ))
-                    }
-                </LinesContainer>
-            </CircleOutlineContainer>
         </RightContainer>
     )
 }
--- src/DashboardUser/pages/WithdrawalHistoryPage.js
+++ src/DashboardUser/pages/WithdrawalHistoryPage.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 import {colors} from "../../constants/colors";
 import { WITHDRAWAL_HISTORY } from "../../constants/DATA";
 
 const WithdrawalHistoryPage = () => {
+    const [ selectedHistory, setSelectedHistory ] = useState({
+        all: true,
+        approved: false,
+        pending: false,
+        failed: false
+    });
+
+    const handleSelect = (type) => {
+        if(type==="all") setSelectedHistory({ all: true, approved: false, pending: false, failed: false });
+        if(type==="approved") setSelectedHistory({ all: false, approved: true, pending: false, failed: false });
+        if(type==="pending") setSelectedHistory({ all: false, approved: false, pending: true, failed: false });
+        if(type==="failed") setSelectedHistory({ all: false, approved: false, pending: false, failed: true });
+        console.log(selectedHistory)
+    }
+
+    const trueItems = WITHDRAWAL_HISTORY.filter(item => {
+        if(selectedHistory.all) return true
+        if(selectedHistory.approved) return item.status === "approved"
+        if(selectedHistory.pending) return item.status === "pending"
+        if(selectedHistory.failed) return item.status === "rejected"
+    })
+
     return(
         <HistoryContainer>
             <HistoryMainTitle>Your withdrawal history</HistoryMainTitle>
             <HistoryBtns>
-                <HistoryBtn>All</HistoryBtn>
-                <HistoryBtn>Approved</HistoryBtn>
-                <HistoryBtn>Pending</HistoryBtn>
-                <HistoryBtn>Failed</HistoryBtn>
-            </HistoryBtns>
+                <HistoryBtn isTrue = {selectedHistory.all} onClick={handleSelect.bind(this, "all")}>All</HistoryBtn>
+                <HistoryBtn isTrue = {selectedHistory.approved} onClick={handleSelect.bind(this, "approved")}>Approved</HistoryBtn>
+                <HistoryBtn isTrue = {selectedHistory.pending} onClick={handleSelect.bind(this, "pending")}>Pending</HistoryBtn>
+                <HistoryBtn isTrue = {selectedHistory.failed} onClick={handleSelect.bind(this, "failed")}>Failed</HistoryBtn>
+            </HistoryBtns> 
             <HistoryDisplay>
-                {WITHDRAWAL_HISTORY.map(item => (
+                {trueItems.map((item, index) => (
                     <HistoryBox key={item.id}>
                         <FromBox><Title>{item.from}</Title><Label>From</Label></FromBox>
                         <AmountBox><Title>{item.amount}</Title><Label>Amount</Label></AmountBox>
@@ -89,6 +111,11 @@ export const HistoryBox = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
     cursor: pointer;
+    transition: all 300ms ease-in-out;
+    :hover {
+        box-shadow: 0 -6px 12px rgba(36,40,80,0.2);
+        transform: scale(1.03);
+    }
 `;
 export const HistoryDisplay = styled.div`
     margin-top: 50px;
@@ -115,14 +142,23 @@ export const HistoryBtns = styled.div`
 export const HistoryBtn = styled.button`
     text-decoration: none;
     border: 2px solid ${colors.accent};
+    border-radius: 4px;
     outline: none;
     padding: 8px 12px;
     width: 100%;
     font-family: Inter, sans-serif;
     text-transform: uppercase;
     font-weight: 700;
-    background-color: ${colors.accent};
-    color: #f5f5f5;
+    transition: all 200ms ease-in-out;
+    /* background-color: ${colors.accent}; */
+    background-color: ${props=> props.isTrue ? colors.accent:"transparent"};
+    /* background-color: transparent; */
+    color: ${props=> !props.isTrue ? colors.accent:"#f5f5f5"};
+    cursor: pointer;
+    :hover{
+        background-color: ${props=> !props.isTrue ? colors.accent:"transparent"};
+        color: ${props=> props.isTrue ? colors.accent:"#f5f5f5"};
+    }
 `;
 
 export default WithdrawalHistoryPage
